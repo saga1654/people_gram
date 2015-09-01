a41e967fff44c64f64aba1b75f2b7c8007b557d8
saga1654 <saga1654@naver.com>
2015-09-01 18:28:35 +0900

    My Question class 추가

--- app/src/main/java/kr/co/people_grame/apppeoplegram/LogoActivity.java
+++ app/src/main/java/kr/co/people_grame/apppeoplegram/LogoActivity.java
@@ -1,36 +1,85 @@
 package kr.co.people_grame.apppeoplegram;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.Window;
+import android.util.Log;
+import android.view.View;
+import android.content.DialogInterface;
+import android.app.AlertDialog;
+import android.view.KeyEvent;
 
 public class LogoActivity extends AppCompatActivity {
+    private Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        //상단 타이틀 제거
-        //import android.view.Window;
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-
         //View
         setContentView(R.layout.activity_logo);
 
-        //import android.os.Handler;
-        //핸들러 이벤트
+        /*
         Handler hd = new Handler();
-
         //핸들러 시작 = 3초 후 종료
         hd.postDelayed(new Runnable() {
             @Override
             public void run() {
+                intent = new Intent(LogoActivity.this, MainActivity.class);
+                startActivity(intent);
+                overridePendingTransition(R.anim.anim_in, R.anim.anim_out);
                 finish();
             }
 
         }, 3000);
+        */
     }
 
+    public void btn_memberJoin(View v)
+    {
+        //Log.d("btn", "회원가입버튼");
+
+
+        intent = new Intent(LogoActivity.this, MyQuestion_Activity.class);
+        startActivity(intent);
+
+    }
+
+    public void btn_memberLogin(View v) {
+        //Log.d("btn", "로그인 버튼");
+
+        intent = new Intent(LogoActivity.this, MyQuestion_Activity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+
+        switch (keyCode) {
+            case KeyEvent.KEYCODE_BACK:
+                new AlertDialog.Builder(this)
+                        .setTitle("프로그램 종료")
+                        .setMessage("프로그램을 종료 하시겠습니까?")
+                        .setPositiveButton("예", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                finish();
+                                moveTaskToBack(true);
+                                android.os.Process.killProcess(android.os.Process.myPid());
+
+                                //android.os.Process.killProcess(android.os.Process.myPid());
+                            }
+                        })
+                        .setNegativeButton("아니오", null)
+                        .show();
+
+                break;
+
+            default:
+                break;
+        }
+
+        return super.onKeyDown(keyCode, event);
+    }
 
 }
--- app/src/main/java/kr/co/people_grame/apppeoplegram/MainActivity.java
+++ app/src/main/java/kr/co/people_grame/apppeoplegram/MainActivity.java
@@ -9,5 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        overridePendingTransition(R.anim.anim_in, R.anim.anim_out);
     }
 }
